9f477f8ab3abc114daf79c29ec238b5e2e746f9f
dnl232 <danconsa@hotmail.com>
2024-11-03 15:20:36 +0100

    forever

--- app/Main.hs
+++ app/Main.hs
@@ -6,7 +6,7 @@ module Main (main) where
 
 import Network.Simple.TCP (serve, HostPreference(..), recv, send)
 import qualified Data.ByteString as BS
-import Control.Monad (void)
+import Control.Monad (void, forever)
 
 
 main :: IO ()
@@ -17,7 +17,7 @@ main = do
     -- Uncomment this block to pass stage 1
     let port = "6379"
     putStrLn $ "Redis server listening on port " ++ port
-    serve (Host "localhost") port $ \(socket, address) -> do
+    serve (Host "localhost") port $ \(socket, address) -> forever $ do
             input <- recv socket 16
             case input of
                 Just x -> void $ send socket "+PONG\r\n"
