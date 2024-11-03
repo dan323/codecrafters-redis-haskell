0258a004c239e457070db4d7f72aa91306db958c
dnl232 <danconsa@hotmail.com>
2024-11-03 15:17:28 +0100

    undo

--- app/Main.hs
+++ app/Main.hs
@@ -4,23 +4,22 @@
 
 module Main (main) where
 
-import Network.Simple.TCP (serve, HostPreference(HostAny), closeSock, recv, send)
+import Network.Simple.TCP (serve, HostPreference(..), recv, send)
 import qualified Data.ByteString as BS
-import Control.Monad (void, forever)
-import Control.Concurrent (forkFinally)
+import Control.Monad (void)
 
 
 main :: IO ()
 main = do
     -- You can use print statements as follows for debugging, they'll be visible when running tests.
-    -- putStrLn "Logs from your program will appear here"
+    putStrLn "Logs from your program will appear here"
 
     -- Uncomment this block to pass stage 1
     let port = "6379"
     putStrLn $ "Redis server listening on port " ++ port
-    forever (serve HostAny port $ \(socket, address) -> do
+    serve (Host "localhost") port $ \(socket, address) -> do
             input <- recv socket 16
             case input of
                 Just x -> void $ send socket "+PONG\r\n"
                 Nothing -> error "fail"
-            putStrLn $ "successfully connected client: " ++ show address)
+            putStrLn $ "successfully connected client: " ++ show address
