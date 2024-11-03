91d672de00990b9a3147aefdf356c8676857c933
dnl232 <danconsa@hotmail.com>
2024-11-03 11:36:43 +0100

    uncomment

--- app/Main.hs
+++ app/Main.hs
@@ -10,11 +10,11 @@ import Network.Simple.TCP (serve, HostPreference(HostAny), closeSock)
 main :: IO ()
 main = do
     -- You can use print statements as follows for debugging, they'll be visible when running tests.
-    putStrLn "Logs from your program will appear here"
+    -- putStrLn "Logs from your program will appear here"
 
     -- Uncomment this block to pass stage 1
-    -- let port = "6379"
-    -- putStrLn $ "Redis server listening on port " ++ port
-    -- serve HostAny port $ \(socket, address) -> do
-    --     putStrLn $ "successfully connected client: " ++ show address
-    --     closeSock socket
+    let port = "6379"
+    putStrLn $ "Redis server listening on port " ++ port
+    serve HostAny port $ \(socket, address) -> do
+        putStrLn $ "successfully connected client: " ++ show address
+        closeSock socket
