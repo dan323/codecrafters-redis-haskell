439b6100ce554ec58ad520cce18c2ae46b410839
dnl232 <danconsa@hotmail.com>
2024-11-08 22:05:05 +0100

    logs

--- app/Main.hs
+++ app/Main.hs
@@ -12,7 +12,7 @@ import qualified Data.Text.Encoding as TSE (decodeUtf8)
 import Network.Simple.TCP (HostPreference (..), recv, send, serve, Socket, SockAddr)
 import Text.Megaparsec (errorBundlePretty, parse)
 import Text.Parser (respParser)
-import qualified Data.Map as M
+import qualified Data.Map as M (Map, empty, insert, delete, lookup)
 import Control.Monad.State (StateT, gets, modify, evalStateT, liftIO, get, lift, put)
 import Data.Functor (($>))
 import Data.Time.Clock (getCurrentTime, UTCTime, addUTCTime, NominalDiffTime)
@@ -66,12 +66,12 @@ interpret date [Command GET, Param (ByteString key)] = do -- gets (maybe NullByt
     map <- get
     let mvalue = M.lookup key map
     case mvalue of
-        Nothing -> return NullByteString
+        Nothing -> error "Data was not inserted" -- return NullByteString
         Just (v, mTime) -> case mTime of
                              Nothing -> return (ByteString v)
                              Just t -> case compare t date of
                                         LT -> return (ByteString v)
-                                        _ -> put (M.delete key map) $> NullByteString
+                                        _ -> put (M.delete key map) $> error ("Data was deleted " ++ show date)
 interpret _ [Command PING] = return $ String "PONG"
 interpret _ [Command ECHO, Param (ByteString b)] = return $ ByteString b
 interpret _ _ = error "Unexpected"
